fe229326efd936f0cd7e94fb99c9df38b14246d2
florent champigny <florent.champigny@backelite.com>
2018-03-18 09:32:25 +0100

    published

--- publish.sh
+++ publish.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 ./gradlew clean assembleDebug install
-./gradlew bintrayUpload
+
+./gradlew :runtime-permission:install
+./gradlew :runtime-permission:bintrayUpload
+
+./gradlew :runtime-permission-kotlin:install
+./gradlew :runtime-permission-kotlin:bintrayUpload
+
+./gradlew :runtime-permission-rx:install
+./gradlew :runtime-permission-rx:bintrayUpload
