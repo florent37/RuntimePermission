5fd8a8f73b78ec6812ee0a444eeeca4bda4a9813
florent champigny <florent.champigny@backelite.com>
2018-10-24 09:29:31 +0200

    published

--- publish.sh
+++ publish.sh
@@ -4,11 +4,11 @@
 ./gradlew assembleDebug
 ./gradlew install
 
-./gradlew :runtime-permission:install
-./gradlew :runtime-permission:bintrayUpload
+./gradlew runtime-permission:install
+./gradlew runtime-permission:bintrayUpload -x runtime-permission-kotlin:bintrayUpload -x runtime-permission-rx:bintrayUpload
 
-./gradlew :runtime-permission-kotlin:install
-./gradlew :runtime-permission-kotlin:bintrayUpload
+./gradlew runtime-permission-kotlin:install
+./gradlew runtime-permission-kotlin:bintrayUpload -x runtime-permission-rx:bintrayUpload -x runtime-permission:bintrayUpload
 
-./gradlew :runtime-permission-rx:install
-./gradlew :runtime-permission-rx:bintrayUpload
+./gradlew runtime-permission-rx:install
+./gradlew runtime-permission-rx:bintrayUpload  -x runtime-permission:bintrayUpload -x runtime-permission-kotlin:bintrayUpload
